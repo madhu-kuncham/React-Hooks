46cbf97481430e724c03b287786fc9f019146138
madhu-kuncham <kmadhukumar88011@gmail.com>
2020-10-12 19:04:44 +0530

    classcounter and hookcounter using useState

--- Hooks/src/App.js
+++ Hooks/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
 
 function App() {
 
   return (
     <div className="App">
-     <h1>Hi , I'm a React app</h1>
+     {/* <ClassCounter /> */}
+     <HookCounter />
     </div>
   );
 
