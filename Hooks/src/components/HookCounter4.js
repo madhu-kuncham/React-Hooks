85ff6f0ec376ff78f76e36a2001f172b7c427432
madhu-kuncham <kmadhukumar88011@gmail.com>
2020-10-12 23:19:20 +0530

    useState with objects

---
+++ Hooks/src/components/HookCounter4.js
@@ -0,0 +1,16 @@
+import React , {useState} from 'react'
+
+function HookCounter4() {
+    const [items ,setItems] = useState([])
+    return (
+        <div>
+            <ul>
+                {
+                    
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default HookCounter4
