2c21b9d6e30d532506b3e280ee74525e344e2cb2
madhu-kuncham <kmadhukumar88011@gmail.com>
2020-10-15 13:10:47 +0530

    individual posts fetching by button clicking using useEffect

--- Hooks/src/components/UseEffectHook/UseEffectFetching.js
+++ Hooks/src/components/UseEffectHook/UseEffectFetching.js
@@ -4,9 +4,14 @@ import axios from 'axios'
 function UseEffectFetching() {
     const [post , setPost] = useState({})
     const [id , setId] = useState(1)
+    const [idFromButtonClick , setIdFromButtonClick] = useState(1)
+
+    const handleClick = () => {
+        setIdFromButtonClick(id)
+    }
 
     useEffect(() => {
-        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
         .then(res => {
             console.log(res)
             setPost(res.data)
@@ -14,11 +19,12 @@ function UseEffectFetching() {
         .catch(err => {
             console.log(err);
         })
-    } , [id])
+    } , [idFromButtonClick])
 
     return (
         <div>
             <input type="text" value ={id} onChange = {e => setId(e.target.value)}/>
+            <button type="button" onClick={handleClick}>Fetch Posts</button>
             <div>
                 {post.title}
             </div>
