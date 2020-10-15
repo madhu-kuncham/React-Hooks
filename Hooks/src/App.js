3c951fc1878f67d8ebe71c74ceefe156af555533
madhu-kuncham <kmadhukumar88011@gmail.com>
2020-10-15 12:34:26 +0530

    data fetching using useEffect

--- Hooks/src/App.js
+++ Hooks/src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
-import HookMouse from './components/UseEffectHook/HookMouse';
-import IntervalHookCounter from './components/UseEffectHook/IntervalHookCounter';
-import MouseContainer from './components/UseEffectHook/MouseContainer';
-import UsEffCounter1 from './components/UseEffectHook/UsEffCounter1';
+import UseEffectFetching from './components/UseEffectHook/UseEffectFetching';
 
 function App() {
 
   return (
     <div className="App">
-      <IntervalHookCounter />
+      <UseEffectFetching />
     </div>
   );
 
