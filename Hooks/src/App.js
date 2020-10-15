b830b55e60a4d8e71642f9710423b7ee8d590aeb
madhu-kuncham <kmadhukumar88011@gmail.com>
2020-10-15 10:58:28 +0530

    UseEffect cleanUp code

--- Hooks/src/App.js
+++ Hooks/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import HookMouse from './components/UseEffectHook/HookMouse';
+import MouseContainer from './components/UseEffectHook/MouseContainer';
 import UsEffCounter1 from './components/UseEffectHook/UsEffCounter1';
 
 function App() {
 
   return (
     <div className="App">
-      <HookMouse />
+      <MouseContainer />
     </div>
   );
 
--- Hooks/src/components/UseEffectHook/HookMouse.js
+++ Hooks/src/components/UseEffectHook/HookMouse.js
@@ -14,7 +14,13 @@ function HookMouse() {
     useEffect(() => {
         console.log('useEffect Called');
         window.addEventListener('mousemove' , logMousePosition)
-    } ,[])
+    
+      return () => {
+        console.log('Component unmoiunting code')
+        window.removeEventListener('mousemove' , logMousePosition)
+      }
+    
+    }  ,[])
 
     return (
         <div>
